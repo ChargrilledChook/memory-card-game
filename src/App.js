46046ed6aa5a1e3b8a9b9eda4b1420e70112b3af
chook <dylanw212@gmail.com>
2021-05-28 10:25:30 +1000

    create basic css / layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
 
 function App() {
-  return <h1>G'day globe</h1>;
+  return (
+    <div className="wrapper">
+      <header>
+        <h1>Memory Card Game</h1>
+        <div className="score">
+          <div>Current Score:</div>
+          <div>High Score:</div>
+        </div>
+      </header>
+      <main>
+        <div className="card">1</div>
+        <div className="card">2</div>
+        <div className="card">3</div>
+        <div className="card">4</div>
+        <div className="card">5</div>
+        <div className="card">6</div>
+        <div className="card">7</div>
+        <div className="card">8</div>
+        <div className="card">9</div>
+        <div className="card">10</div>
+        <div className="card">11</div>
+        <div className="card">12</div>
+      </main>
+      <footer>By Chook</footer>
+    </div>
+  );
 }
 
 export default App;
