b2ccccde4f6fff867340998bd91c6d446904764c
chook <dylanw212@gmail.com>
2021-06-15 14:45:37 +1000

    destructure props and add comment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
-export default function Header(props) {
+export default function Header({ score, highScore }) {
   return (
     <header>
       <div>
         <h1>Memory Cards</h1>
         <div>Pick a card, any card - but don't click the same card twice!</div>
       </div>
-      {/* <div className="theme">
+      {/* Was the basis for a theme picker or ability to change image sets. Leaving in for now in case
+      I choose to revist since the html here for it is largely fine
+      <div className="theme">
         <label htmlFor="theme">Choose theme:</label>
         <select name="theme" id="theme">
           <option value="cards">Cards</option>
@@ -13,8 +15,8 @@ export default function Header(props) {
         </select>
       </div> */}
       <div className="score">
-        <div>Current Score: {props.score}</div>
-        <div>Highest Score: {props.highScore}</div>
+        <div>Current Score: {score}</div>
+        <div>Highest Score: {highScore}</div>
       </div>
     </header>
   );
