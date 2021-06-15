68e587f3b0417345f635eae951c30e35ae5dfd4c
chook <dylanw212@gmail.com>
2021-06-15 14:46:47 +1000

    fix comment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,15 +5,6 @@ export default function Header({ score, highScore }) {
         <h1>Memory Cards</h1>
         <div>Pick a card, any card - but don't click the same card twice!</div>
       </div>
-      {/* Was the basis for a theme picker or ability to change image sets. Leaving in for now in case
-      I choose to revist since the html here for it is largely fine
-      <div className="theme">
-        <label htmlFor="theme">Choose theme:</label>
-        <select name="theme" id="theme">
-          <option value="cards">Cards</option>
-          <option value="dogs">My Dogs</option>
-        </select>
-      </div> */}
       <div className="score">
         <div>Current Score: {score}</div>
         <div>Highest Score: {highScore}</div>
@@ -21,3 +12,13 @@ export default function Header({ score, highScore }) {
     </header>
   );
 }
+
+/* Below was the basis for a theme picker or ability to change image sets.
+Leaving in for now in case I choose to revist since the html here for it is largely fine
+<div className="theme">
+  <label htmlFor="theme">Choose theme:</label>
+  <select name="theme" id="theme">
+    <option value="cards">Cards</option>
+    <option value="dogs">My Dogs</option>
+  </select>
+</div> */
